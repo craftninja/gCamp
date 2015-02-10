77fb4f0247947d3125e0789ebfc1586f983dd1e3
EmilyPlatzer <emilyplatzer@gmail.com>
2015-02-10 15:22:08 -0700

    User can see about page

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -11,4 +11,19 @@ describe 'Welcome pages -' do
       expect(page).to have_content('Your life, organized.')
     end
   end
+
+  scenario 'User can see the about page' do
+    copy = "We are dedicated to the makers, the creators, the risk-takers. We wake up in the middle of the night and furiously write down ideas about how to make your online life better. Human collaboration and communication is what drives us, and it infuses every feature of gCamp."
+
+    visit '/'
+    click_on 'About'
+    within '.page-header' do
+      expect(page).to have_content('About')
+    end
+    within '.row' do
+      within '.col-md-8' do
+        expect(page).to have_content(copy)
+      end
+    end
+  end
 end
