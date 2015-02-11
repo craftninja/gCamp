ab17cb488a681d7b89234f4884f60ea0d6c83686
EmilyPlatzer <emilyplatzer@gmail.com>
2015-02-11 09:32:39 -0700

    User can see quotes on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
+    @quotes = {
+      'Carla Hayes' => "gCamp has changed my life! It's the best tool I've ever used.",
+      'Leta Jaskolski' => "Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been.",
+      'Laverne Upton' => "Don't hesitate - sign up right now! You'll never be the same.",
+    }
   end
 
   def about
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe 'Welcome pages -' do
   scenario 'User is welcomed on homepage' do
+    quotes = "gCamp has changed my life! It's the best tool I've ever used. - Carla Hayes Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been. - Leta Jaskolski Don't hesitate - sign up right now! You'll never be the same. - Laverne Upton"
     visit '/'
     within '.navbar' do
       expect(page).to have_content('gCamp')
@@ -10,6 +11,7 @@ describe 'Welcome pages -' do
       expect(page).to have_content('gCamp')
       expect(page).to have_content('Your life, organized.')
     end
+    expect(page).to have_content(quotes)
   end
 
   scenario 'User can see the about page' do
