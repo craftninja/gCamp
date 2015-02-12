6629e448c651a4583e2c7d5cf5d632a5b922c9c9
EmilyPlatzer <emilyplatzer@gmail.com>
2015-02-12 16:24:00 -0700

    Users can update users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -23,4 +23,21 @@ feature 'Users -' do
     expect(page).to have_link(email)
   end
 
+  scenario 'Users can update users' do
+    fname = 'Luke'
+    lname = 'Bartel'
+
+    visit '/users'
+    click_on 'New User'
+    fill_in 'First Name', with: fname
+    fill_in 'Last Name', with: lname
+    fill_in 'Email', with: "#{fname}@example.com"
+    click_on 'Create User'
+    click_on 'Edit'
+    fill_in 'Email', with: "#{lname}@example.com"
+    click_on 'Update User'
+    expect(page).to have_content("#{lname}@example.com")
+    expect(page).to_not have_content("#{fname}@example.com")
+  end
+
 end
